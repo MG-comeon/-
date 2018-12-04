69f80db2ebebbd0b49786b695f99147261ef369a
DESKTOP-6PMON51\AAA <mmmcomeon@193.com>
2018-12-04 11:43:39 +0800

    内容补充

--- api.js
+++ api.js
@@ -17,7 +17,7 @@ const getData = (url, params)=>{
   })
   return promise;
 }
-let url = 'https://dev.hz139.com'
+let url = 'https://dev.hz13923.com'
 // let url = 'https://api.hz139.com'
 // 登陆
 const login = params => getData(url + '/portal/user/weixin/login', params)
@@ -26,3 +26,37 @@ module.exports = {
   login,
   bind
 }
+// 接口用例 需注释
+let params = {
+  method: 'POST',
+  data: {
+    code: res.code,
+    userType: 2
+  }
+}
+api.login(params).then(res => {
+  wx.hideLoading()
+  console.log(res)
+  if (res.statusCode == 200) {
+    switch (parseInt(res.data.code)) {
+      case 200:
+        // 登陆成功要做的
+
+        break;
+      default:
+        //异常数据返回 
+        wx.showToast({
+          title: res.data.code + res.data.message,
+          icon: 'none',
+          duration: 2000
+        })
+    }
+  } else {
+    return res.statusCode
+  }
+}).catch(res => {
+  wx.showToast({
+    title: '网络错误' + res,
+    icon: 'none'
+  })
+})
